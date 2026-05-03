03d8d5999a46b882d87e107880a431587589c967
Qanix <Qanix@outlook.it>
2026-05-03 17:20:26 +0800

    fix: 修正LC_MESSAGES的语言设置为zh_CN.UTF-8

--- modules/nixos/i18n/locale.nix
+++ modules/nixos/i18n/locale.nix
@@ -16,7 +16,7 @@
       LC_PAPER          = "zh_CN.UTF-8";
       LC_TELEPHONE      = "zh_CN.UTF-8";
       LC_TIME           = "zh_CN.UTF-8";
-      LC_MESSAGES       = "zn_CN.UTF-8";
+      LC_MESSAGES       = "zh_CN.UTF-8";
     };
   };
 }
