8e0bb8a8bd42af578a39760b4d912adf1c57bdd8
Qanix <Qanix@outlook.it>
2026-05-03 16:25:16 +0800

    fix: 注释掉XDG_CURRENT_DESKTOP和XDG_SESSION_DESKTOP配置

--- modules/nixos/core/environment.nix
+++ modules/nixos/core/environment.nix
@@ -25,8 +25,8 @@
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     XDG_SESSION_TYPE   = "wayland";
-    XDG_CURRENT_DESKTOP = "niri";
-    XDG_SESSION_DESKTOP = "niri";
+    # XDG_CURRENT_DESKTOP = "niri";
+    # XDG_SESSION_DESKTOP = "niri";
   };
 
   environment.systemPackages = with pkgs; [
